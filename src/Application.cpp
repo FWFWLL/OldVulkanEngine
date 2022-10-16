baa9782df66726cc5b658ac7963029e6d0b976e2
FWFWLL <ffl@vivaldi.net>
2022-10-17 00:03:14 +0200

    Vulkan Tutorial 21

--- src/Application.cpp
+++ src/Application.cpp
@@ -35,8 +35,10 @@
 namespace FFL {
 
 struct GlobalUniformBufferObject {
-	alignas(16) glm::mat4 projectionView{1.0f};
-	alignas(16) glm::vec3 lightDirection = glm::normalize(glm::vec3{1.0f, -3.0f, -1.0f});
+	glm::mat4 projectionView{1.0f};
+	glm::vec4 ambientLightColor{1.0f, 1.0f, 1.0f, 0.02f}; // W is intensity
+	glm::vec3 lightPosition{-1.0f}; // Ignore W
+	alignas(16) glm::vec4 lightColor{1.0f}; // W is light intensity
 };
 
 Application::Application() {
@@ -73,6 +75,7 @@ void Application::run() {
 	Camera camera{};
 
 	GameObject viewerObject = GameObject::createGameObject();
+	viewerObject.transform.translation.z = -2.5f;
 	KeyboardMovementController cameraController{};
 
 	auto currentTime = std::chrono::high_resolution_clock::now();
@@ -90,7 +93,7 @@ void Application::run() {
 		camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
 
 		float aspect = m_renderer.getAspectRatio();
-		camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 10.0f);
+		camera.setPerspectiveProjection(glm::radians(50.0f), aspect, 0.1f, 100.0f);
 
 		if(VkCommandBuffer commandBuffer = m_renderer.beginFrame()) {
 			int frameIndex = m_renderer.getFrameIndex();
@@ -118,7 +121,7 @@ void Application::loadGameObjects() {
 
 	GameObject flatVase = GameObject::createGameObject();
 	flatVase.model = model;
-	flatVase.transform.translation = {-0.5f, 0.5f, 2.5f};
+	flatVase.transform.translation = {-0.5f, 0.5f, 0.0f};
 	flatVase.transform.scale = {3.0f, 1.5f, 3.0f};
 
 	m_gameObjects.push_back(std::move(flatVase));
@@ -127,10 +130,19 @@ void Application::loadGameObjects() {
 
 	GameObject smoothVase = GameObject::createGameObject();
 	smoothVase.model = model;
-	smoothVase.transform.translation = {0.5f, 0.5f, 2.5f};
+	smoothVase.transform.translation = {0.5f, 0.5f, 0.0f};
 	smoothVase.transform.scale = {3.0f, 1.5f, 3.0f};
 
 	m_gameObjects.push_back(std::move(smoothVase));
+
+	model = Model::createModelFromFile(m_device, "models/quad.obj");
+
+	GameObject floor = GameObject::createGameObject();
+	floor.model = model;
+	floor.transform.translation = {0.0f, 0.5f, 0.0f};
+	floor.transform.scale = {3.0f, 1.0f, 3.0f};
+
+	m_gameObjects.push_back(std::move(floor));
 }
 
 } // FFL
